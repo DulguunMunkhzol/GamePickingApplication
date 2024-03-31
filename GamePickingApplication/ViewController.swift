91bf8705eea278787ba8ff093af2b570e0b79298
dulguun <m.dulguunuu@gmail.com>
2024-03-31 16:40:38 -0400

    Initial Commit

---
+++ GamePickingApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GamePickingApplication
+//
+//  Created by Dulguun Munkhzol on 3/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
